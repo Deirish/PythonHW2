91bffc8629fbe5ae0ec838b74e08bca8577446af
Irina <lykin4ik@gmail.com>
2022-10-21 17:21:34 +0300

    Homework

---
+++ lesson2_hw2_1.py
@@ -0,0 +1,8 @@
+# оптимальный возраст(для какой то вакансии, например)
+your_age = int(input())
+if ((your_age >= 18) and (your_age <= 55)):
+    print("Welcome!")
+elif (your_age < 18):
+    print("Not this time.")
+else:
+     print("Sorry!")
